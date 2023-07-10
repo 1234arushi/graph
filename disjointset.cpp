f2e3317d36e23c55886e2047ef6f6e73e90bd730
arushisharma <39919147+1234arushi@users.noreply.github.com>
2023-07-10 10:31:11 +0530

    Update disjointset.cpp

--- disjointset.cpp
+++ disjointset.cpp
@@ -1,16 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 class DisjointSet{
-    vector<int>rankk,parent;
+    vector<int>rankk,parent,sizee;
 public:
     DisjointSet(int n)
     {
 
         rankk.resize(n+1,0);
         parent.resize(n+1);
+        sizee.resize(n+1);
         for(int i=0;i<=n;i++)
         {
             parent[i]=i;
+            sizee[i]=1;
         }
     }
     int findupar(int node)
@@ -44,18 +46,39 @@ public:
             rankk[ulp_v]++;
         }
     }
+    void unionbysize(int u,int v)
+    {
+        int ulp_u=findupar(u);
+        int ulp_v=findupar(v);
+        if(ulp_u == ulp_v)return;
+        if(sizee[ulp_u] < sizee[ulp_v])
+        {
+
+            parent[ulp_u]=ulp_v;
+            sizee[ulp_v]+=sizee[ulp_u];
+        }
+        else
+        {
+             parent[ulp_v]=ulp_u;
+            sizee[ulp_u]+=sizee[ulp_v];
+
+
+        }
+
+
+    }
 };
 int main()
 
 {
 
     DisjointSet ds(7);
-    ds.unionbyrank(1,2);
-    ds.unionbyrank(2,3);
-    ds.unionbyrank(4,5);
-    ds.unionbyrank(6,7);
-    ds.unionbyrank(5,6);
-    ds.unionbyrank(1,2);
+    ds.unionbysize(1,2);
+    ds.unionbysize(2,3);
+    ds.unionbysize(4,5);
+    ds.unionbysize(6,7);
+    ds.unionbysize(5,6);
+    ds.unionbysize(1,2);
     if(ds.findupar(3)==ds.findupar(7))
     {
 
@@ -64,5 +87,14 @@ int main()
     else{
         cout<<"not same"<<endl;
     }
-    ds.unionbyrank(3,7);
+    ds.unionbysize(3,7);
+    if(ds.findupar(3)==ds.findupar(7))
+    {
+
+        cout<<"same"<<endl;
+    }
+    else{
+        cout<<"not same"<<endl;
+    }
+    return 0;
 }
